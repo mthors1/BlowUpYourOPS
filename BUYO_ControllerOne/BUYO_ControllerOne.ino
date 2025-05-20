a9574125e3dabff763a61bd114c6dec06e4a735a
Sayeed Azam <sayeed.azam838@gmail.com>
2025-05-20 13:17:26 -0400

    transferred old data to new document, additional comments

--- BUYO_ControllerOne/BUYO_ControllerOne.ino
+++ BUYO_ControllerOne/BUYO_ControllerOne.ino
@@ -1,5 +1,3 @@
-// Test comment
-
 /* Define Variables */
 
 // Maze game pins
@@ -30,6 +28,226 @@ int wireWinLED = 12; // digital pin for win LED
 int correctWire; // initialize variable for randomly generated correct wire
 bool wirePulled[4]; // array to keep track of which wires have been pulled
 
+// Speaker & sound variables
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
+int note_dur = 30; // duration of music notes 
+
+
+/* Speaker and music functions*/
+void play(int note, int dur) {
+  tone(speaker, note);
+  delay(dur * note_dur);
+  noTone(speaker);
+  delay(dur * note_dur / 3);
+}
+
+
+void successSound() {
+  play(NOTE_E4, 1);
+  play(NOTE_G4, 1);
+  play(NOTE_C5, 2);
+}
+
+
+void failureSound() {
+  play(NOTE_C3, 3);
+  play(NOTE_E3, 2);
+  play(NOTE_A3, 1);
+}
+
+
+void song3() {
+  play(NOTE_DS4, 2);
+  play(NOTE_E4, 2);
+  play(NOTE_F4, 5);
+  play(NOTE_F4, 2);
+  play(NOTE_FS4, 2);
+  play(NOTE_G4, 5);
+  play(NOTE_G4, 2);
+  play(NOTE_GS4, 2);
+  play(NOTE_A4, 5);
+  play(NOTE_C5, 9);
+}
+
+/* Maze game functions */
+
+int readJoystick() {
+  // Map potentiometer readings to 1-100 scale
+  xpos = map(analogRead(X_pin), 0, 1023, 1, 100);
+  ypos = map(analogRead(Y_pin), 0, 1023, 1, 100);
+
+  // Based on 1-100 scale, read directional inputs
+  if (xpos < 40) { return -1; } // go left
+  if (xpos > 60) { return 1; }  // go right
+  if (ypos < 40) { return -2; } // go up
+  if (ypos > 60) { return 2; }  // go down
+  return 0; // no movement
+}
+
+// Maze generation process developed with the help of AI tools
+
+bool visited[maze_size][maze_size];
+
+bool isSolvableDFS(int x, int, y) {
+  if (x < 0 || x > maze_size || y < 0 || y >= maze_size) return false;
+  if (maze[x][y] == 1 || visited[x][y]) return false;
+  if (x == goalX && y == goalY) return true;
+
+  visited[x][y] == true;
+
+  return isSolvableDFS(x + 1, y) ||
+         isSolvableDFS(x - 1, y) ||
+         isSolvableDFS(x, y + 1) ||
+         isSolvableDFS(x, y - 1);
+}
+
+bool mazeIsSolvable() { 
+  memset(visited, 0, sizeof(visited));
+  return isSolvableDFS(playerX, playerY);
+}
+
+void generateMaze() {
+  do {
+    randomSeed(micros());
+    for (int i = 0; i < maze_size; i++) {
+      for (int j = 0; j < naze_size; j++) {
+        maze[i][j] = (random(0, 100) < 30) ? 1 : 0;
+      }
+    }
+    maze[playerX][playerY] = 0;
+    maze[goalX][goalY] = 0;
+  } while (!mazeIsSolvable());
+}
+
+void resetGame() {
+  Serial.println("Resetting game...");
+
+  // reset player position
+  playerX = 0;
+  playerY = 0;
+
+  // clear visited array
+  for (int i = 0; i < maze_size; i++){
+    for (int j = 0; j < maze_size; j++){
+      if (random(0, 100) < 30){
+        maze[i][j] = 1;
+      }
+      else{
+        maze[i][j] = 0;
+      }
+    }
+  }
+  
+  // reset player and goal positions
+  maze[playerX][playerY] = 0;
+  maze[goalX][goalY] = 0;
+  while (!mazeIsSolvable());
+}
+
+bool checkWin() {
+  if (playerX == goalX && playerY == goalY) {
+    return true;
+  }
+  return false;
+}
+
+bool mazeWinner() {
+  if (checkWin()) {
+    delay(500);
+    song3();
+    return true;
+  }
+  return false;
+}
 
 
 void setup() {

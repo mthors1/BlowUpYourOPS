3f86667165ceba42f3dc45e68cb2be3067e7effd
Sayeed Azam <sayeed.azam838@gmail.com>
2026-05-19 16:32:48 -0400

    Created .ino files for both Games and Displays MCU's

---
+++ ArduinoPrototypingSTM32/MCU_Displays/MCU_Displays/MCU_Displays.ino
@@ -0,0 +1,11 @@
+// LCD, Timer, Piezo, start/reset functionality
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ ArduinoPrototypingSTM32/MCU_Games/MCU_Games.ino
@@ -0,0 +1,40 @@
+// Define Pins
+
+
+// Initialize variables
+bool started = true; // Should be initialized as false, only true for testing
+bool memoryGame = false;
+bool colorGame = false;
+
+
+
+void setup() {
+  
+  
+
+}
+
+void loop() {
+  // while (!started):
+
+  '''
+  -- Game loop functionality
+  if (!Game1){
+    buttonGame();
+  }
+  else if (!Game2){
+
+  }
+  ...
+ '''
+
+}
+
+
+buttonGame{
+  while (notDone){
+    if (userWins){
+      notDone = false;
+    }
+  }
+}

aded8e06b08ebe604806d86e281ae6a7ce0014a8
Sayeed Azam <sayeed.azam838@gmail.com>
2026-06-07 16:07:08 -0400

    Wrote loop for wire game

--- ArduinoPrototypingSTM32/ModularCode/WireGame/WireGame/WireGame.ino
+++ ArduinoPrototypingSTM32/ModularCode/WireGame/WireGame/WireGame.ino
@@ -1,19 +1,118 @@
+#include "pitches.h"
+HardwareSerial Serial2(PA3, PA2);
+
 // Wire game pins
-const int wirePins[4] = {2, 3, 4, 5}; // array for digital pins connected to pull-wires
-int wireWinLED = 12; // digital pin for win LED
+const int wirePins[4] = {PE4, PE5, PB4, PB5}; // array for digital pins connected to pull-wires
 
 // Wire game variables
 int correctWire; // initialize variable for randomly generated correct wire
 bool wirePulled[4]; // array to keep track of which wires have been pulled
 
+unsigned long printTimer;
+unsigned long currentTime;
 
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial2.begin(115200);
+  printWireStatus();
+
+  for (int i = 0; i < 4; i++) {
+    pinMode(wirePins[i], INPUT_PULLUP);
+    wirePulled[i] = false;
+    Serial2.print("Wire ");
+    Serial2.print(i);
+    Serial2.print(" state: ");
+    //Serial.println(digitalRead(wirePins[i]));
+  }
+  randomSeed(micros());
+  correctWire = random(0, 4);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void printWireStatus(){
+  Serial2.println("Wire signals:");
+    for (int i = 0; i < 4; i++){
+      Serial2.print("Wire ");
+      Serial2.print(i);
+      Serial2.print(": ");
+      Serial2.println(digitalRead(wirePins[i]));
+    }
 
+    Serial2.println();
+
+    Serial2.println("Wire pulled statuses:");
+    for (int i = 0; i < 4; i++){
+      Serial2.print("Wire ");
+      Serial2.print(i);
+      Serial2.print(": ");
+      Serial2.println(wirePulled[i]);
+    }
+
+    Serial2.println();
+}
+
+bool wireGame(){
+  // Debugging
+  Serial2.print("Game started. Pull correct wire to win. Correct wire: ");
+  Serial2.println(correctWire + 1);
+
+  bool snippingWires = true;
+  bool replacingWires = false;
+
+  printTimer = millis();
+  while (snippingWires){
+    for (int i = 0; i < 4; i++){
+      if (digitalRead(wirePins[i]) == HIGH && wirePulled[i] == false){
+        wirePulled[i] = true;
+
+        if (i == correctWire){
+          Serial2.println("Correct wire Pulled");
+          snippingWires = false;
+          song3();
+        }
+        else{
+          Serial2.println("Wrong wire pulled");
+          wrongWireSound();
+          //Send signal to subtract time
+        }
+      }
+    }
+    // Debugging
+    currentTime = millis();
+    if (currentTime - printTimer > 1500){
+      printTimer = millis();
+      printWireStatus();
+    }
+  }
+
+  int wiresReplaced = 0;
+  printTimer = millis();
+  while (replacingWires){
+    for (int i = 0; i < 4; i++){
+      if (digitalRead(wirePins[i]) == LOW && wirePulled[i] == true){
+        wirePulled[i] = false;
+        wiresReplaced++;
+      }
+    }
+    if (wiresReplaced == 4){
+      replacingWires = false;
+    }
+
+    //debugging
+    currentTime = millis();
+    if (currentTime - printTimer > 1500){
+      printTimer = millis();
+      printWireStatus();
+    }
+  }
+  Serial2.println("Wire game completed");
+  delay(5000);
+
+
+  return true;
+}
+
+
+void loop() {
+  wireGame();
 }
---
+++ ArduinoPrototypingSTM32/ModularCode/WireGame/WireGame/pitches.h
@@ -0,0 +1,157 @@
+/*************************************************
+ * Public Constants
+ *************************************************/
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
+
+const int speaker = PC8;
+int NOTE_DUR = 30; // NOT TRANSFERRED
+// Speaker functions
+
+void play(int note, int dur) {
+  tone(speaker, note);
+  delay(dur * NOTE_DUR);
+  noTone(speaker);
+  delay(dur * NOTE_DUR / 3);
+}
+
+
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 100L; i += tone * 2) {
+    digitalWrite(speaker, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(speaker, LOW);
+    delayMicroseconds(tone);
+  }
+}
+
+
+void wrongWireSound(){
+  play(NOTE_C3, 3);
+  play(NOTE_E3, 2);
+  play(NOTE_A3, 1);
+}
+
+void successSound() {
+  play(NOTE_E4, 1);
+  play(NOTE_G4, 1);
+  play(NOTE_C5, 2);
+}
+
+void resetSound() {
+  play(NOTE_D5, 2);
+  play(NOTE_E5, 2);
+  play(NOTE_F5, 2);
+  play(NOTE_G5, 4);
+  //play(NOTE_F5, 2);
+}
+
+void failureSound() {
+  play(NOTE_C3, 3);
+  play(NOTE_E3, 2);
+  play(NOTE_A3, 1);
+}
+
+
+void song3() {
+  play(NOTE_DS4, 2);
+  play(NOTE_E4, 2);
+  play(NOTE_F4, 5);
+  play(NOTE_F4, 2);
+  play(NOTE_FS4, 2);
+  play(NOTE_G4, 5);
+  play(NOTE_G4, 2);
+  play(NOTE_GS4, 2);
+  play(NOTE_A4, 5);
+  play(NOTE_C5, 9);
+}
+

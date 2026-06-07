b1da3c2daaed9f9d6dcb280b2b09a19999d0c402
Sayeed Azam <sayeed.azam838@gmail.com>
2026-06-07 14:40:04 -0400

    Created module file for Wire Game

---
+++ ArduinoPrototypingSTM32/ModularCode/WireGame/WireGame/WireGame.ino
@@ -0,0 +1,19 @@
+// Wire game pins
+const int wirePins[4] = {2, 3, 4, 5}; // array for digital pins connected to pull-wires
+int wireWinLED = 12; // digital pin for win LED
+
+// Wire game variables
+int correctWire; // initialize variable for randomly generated correct wire
+bool wirePulled[4]; // array to keep track of which wires have been pulled
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
